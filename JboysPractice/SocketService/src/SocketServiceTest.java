5edf15c9812c9f82c08b6ea102fe5fb07811359d
Jboy Flaga <flaga.jeremiah@gmail.com>
2017-12-24 19:09:35 +0800

    failing test for sending message

--- JboysPractice/SocketService/src/SocketServiceTest.java
+++ JboysPractice/SocketService/src/SocketServiceTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,6 +26,22 @@ class SocketServiceTest {
             connect(999);
         socketService.close();
         assertEquals(10, socketService.connections());
+    }
+
+    @Test
+    public void testSendMessage() throws Exception {
+        SocketService socketService = new SocketService();
+        socketService.serve(999, new HelloServer());
+        Socket socket = new Socket("localhost", 999);
+
+        InputStream inputStream = socket.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String answer = bufferedReader.readLine();
+        socket.close();
+
+        assertEquals("Hello", answer);
     }
 
     private void connect(int port) {
