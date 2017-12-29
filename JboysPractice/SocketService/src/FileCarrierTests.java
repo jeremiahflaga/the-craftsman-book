8cfee7d3e90900059c5f0f6e8070622c67a220ec
Jboy Flaga <flaga.jeremiah@gmail.com>
2017-12-30 06:00:17 +0800

    test that "ensures that FileCarrier faithfully reproduces the file" -- passes

--- JboysPractice/SocketService/src/FileCarrierTests.java
+++ JboysPractice/SocketService/src/FileCarrierTests.java
@@ -6,32 +6,95 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FileCarrierTests {
+
     @Test
     public void testAFile() throws Exception {
-        final String TESTFILE = "testFile.txt";
-        final String TESTSTRING = "test";
+        final String ORIGINAL_FILENAME = "testFileCarrier.txt";
+        final String RENAMED_FILENAME = "testFileCarrierRenamed.txt";
+        File originalFile = new File(ORIGINAL_FILENAME);
+        File renamedOriginal = new File(RENAMED_FILENAME);
 
-        createFile(TESTFILE, TESTSTRING);
+        ensureFileIsRemoved(originalFile);
+        ensureFileIsRemoved(renamedOriginal);
 
-        FileCarrier fileCarrier = new FileCarrier(TESTFILE);
-        new File(TESTFILE).delete();
-        fileCarrier.write();
+        createTestFile(originalFile);
+        FileCarrier fc = new FileCarrier(ORIGINAL_FILENAME);
+        rename(originalFile, renamedOriginal);
+        fc.write();
 
-        assertTrue(new File(TESTFILE).exists());
+        assertTrue(originalFile.exists());
+        assertTrue(filesAreTheSame(originalFile, renamedOriginal));
 
-        String contents = readFile(TESTFILE);
-        assertEquals(TESTSTRING, contents);
+        originalFile.delete();
+        renamedOriginal.delete();
+
+
+
+//        final String TESTFILE = "testFile.txt";
+//        final String TESTSTRING = "test";
+//
+//        createFile(TESTFILE, TESTSTRING);
+//
+//        FileCarrier fileCarrier = new FileCarrier(TESTFILE);
+//        new File(TESTFILE).delete();
+//        fileCarrier.write();
+//
+//        assertTrue(new File(TESTFILE).exists());
+//
+//        String contents = readFile(TESTFILE);
+//        assertEquals(TESTSTRING, contents);
     }
 
-    private String readFile(final String TESTFILE) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(TESTFILE));
-        String line = reader.readLine();
-        return line;
+    private boolean filesAreTheSame(File file1, File file2) throws Exception {
+        FileInputStream stream1 = new FileInputStream(file1);
+        FileInputStream stream2 = new FileInputStream(file2);
+
+        try {
+            int c;
+            while ((c = stream1.read()) != -1) {
+                if (stream2.read() != c) {
+                    return false;
+                }
+            }
+            if (stream2.read() != -1)
+                return false;
+            else
+                return true;
+        } finally {
+            stream1.close();
+            stream2.close();
+        }
     }
-    private void createFile(final String TESTFILE,
-                            final String TESTSTRING) throws IOException {
-        PrintWriter writer = new PrintWriter(new FileWriter(TESTFILE));
-        writer.println(TESTSTRING);
+
+    private void rename(File oldFile, File newFile) {
+        oldFile.renameTo(newFile);
+        assertTrue(oldFile.exists() == false);
+        assertTrue(newFile.exists());
+    }
+
+    private void createTestFile(File file) throws IOException {
+        PrintWriter writer = new PrintWriter(new FileWriter(file));
+        writer.println("line one");
+        writer.println("line two");
+        writer.println("line three");
         writer.close();
     }
+
+    private void ensureFileIsRemoved(File file) {
+        if (file.exists()) file.delete();
+        assertTrue(file.exists() == false);
+    }
+
+//    private String readFile(final String TESTFILE) throws IOException {
+//        BufferedReader reader = new BufferedReader(new FileReader(TESTFILE));
+//        String line = reader.readLine();
+//        return line;
+//    }
+//
+//    private void createFile(final String TESTFILE,
+//                            final String TESTSTRING) throws IOException {
+//        PrintWriter writer = new PrintWriter(new FileWriter(TESTFILE));
+//        writer.println(TESTSTRING);
+//        writer.close();
+//    }
 }
